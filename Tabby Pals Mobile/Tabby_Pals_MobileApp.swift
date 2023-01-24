0b770eb3c1b06c7222f85ab52f27fd0259b33b26
borna471 <borna471@gmail.com>
2023-01-23 22:12:56 -0800

    Initial Commit

---
+++ Tabby Pals Mobile/Tabby_Pals_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabby_Pals_MobileApp.swift
+//  Tabby Pals Mobile
+//
+//  Created by Borna Shani on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Tabby_Pals_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
